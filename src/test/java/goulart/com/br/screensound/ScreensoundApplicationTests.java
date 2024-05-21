ca33e51d649a2a4e2e22d0c37f9cf6f02f2c3248
LGoulart <leandrovgoulart@gmail.com>
2024-05-21 14:53:14 -0300

    first commit

---
+++ src/test/java/goulart/com/br/screensound/ScreensoundApplicationTests.java
@@ -0,0 +1,13 @@
+package goulart.com.br.screensound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreensoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
